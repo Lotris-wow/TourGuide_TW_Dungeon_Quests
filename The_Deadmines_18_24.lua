24efb91cebb653923d6f824d4d563b371579d059
Lotris-wow <163733100+Lotris-wow@users.noreply.github.com>
2024-03-17 15:19:30 +0200

    Initial
    Add first guide for Deadmines quests

---
+++ The_Deadmines_18_24.lua
@@ -0,0 +1,106 @@
+TourGuide:RegisterGuide("The Deadmines (18-24)", nil, "Alliance",function()
+
+return [[
+
+N Level 16 Required |N|You need to be at least level 16 to continue this guide
+
+F Fly to Ironforge |QID|2041| |N| Fly to Ironforge (55.5, 47.7)| |Z|Ironforge| |O|
+R Run to Gnoarn  |QID|2041| |N| Run to Gnoarn (69.6, 50.6)| |Z|Ironforge| |O|
+A Accept Speak with Shoni |QID|2041| |N| Accept Speak with Shoni at (69.6, 50.6)| |Z|Ironforge| |O|
+R Run to Shoni the Shilent |QID|2041| |N| Run to Shoni the Shilent in Stormwind City at (62.6, 34.1)| |Z|Stormwind City| |O| |REACH|
+T Turn-In **Speak with Shoni** |QID|2041| |N| Speak with Shoni the Shilent in Stormwind City at (62.6, 34.1)| |Z|Stormwind City| |O|
+
+A Underground Assault |QID|2040| |N|Speak with Shoni the Shilent and Accept Underground Assault (62.60, 34.10)| |Z|Stormwind City| |REACH|
+
+R Run to Gryan Stoutmantle in Westfall |QID|65| |N| Run to Gryan Stoutmantle in Westfall (56.2, 47.6)| |Z|Westfall| |REACH|
+A Accept The Defias Brotherhood Part 1.1 |QID|65| |N| Accept The Defias Brotherhood Part 1.1| |Z|Westfall|
+F Fly to Lakeshire |QID|65| |N| Fly to Lakeshire (30.6, 59.6)| |Z|Redridge Mountains|
+R Run to Wiley the Black |QID|65| |N| Run to Wiley the Black (26.6, 45.2)| |Z|Redridge Mountains|
+T Turn-In The Defias Brotherhood Part 1.1 |QID|65| |N| Turn-In The Defias Brotherhood Part 1.1 (26.6, 45.2)| |Z|Redridge Mountains|
+
+A The Defias Brotherhood Part 1.2 |QID|132| |N| Accept The Defias Brotherhood Part 1.2 (26.6, 45.2)| |Z|Redridge Mountains|
+R The Defias Brotherhood Part 1.2 |QID|132| |N| Run to the Flypoint (30.6, 59.6)| |Z|Redridge Mountains|
+F The Defias Brotherhood Part 1.2 |QID|132| |N| Fly to Gryan Stoutmantle (56.2, 47.6)| |Z|Westfall|
+T The Defias Brotherhood Part 1.2 |QID|132| |N| Turn-In The Defias Brotherhood Part 1.2 (56.2, 47.6)| |Z|Westfall|
+
+A The Defias Brotherhood Part 1.3 |QID|135| |N| Accept The Defias Brotherhood Part 1.3 (56.2, 47.6)| |Z|Westfall|
+F The Defias Brotherhood Part 1.3 |QID|135| |N| Fly to the Stormwind City (66.2, 62.4)| |Z|Stormwind City |
+R The Defias Brotherhood Part 1.3 |QID|135| |N| Run to Master Mathias Shaw in Stormwind City (75.8, 59.8)| |Z|Stormwind City|
+T The Defias Brotherhood Part 1.3 |QID|135| |N| Turn-In The Defias Brotherhood Part 1.3(75.8, 59.8)| |Z|Stormwind City|
+
+A The Defias Brotherhood Part 1.4 |QID|141| |N| Accept The Defias Brotherhood Part 1.4(75.8, 59.8)| |Z|Stormwind City|
+R The Defias Brotherhood Part 1.4 |QID|141| |N| Run to Flypoint (66.2, 62.4)| |Z|Stormwind City|
+F The Defias Brotherhood Part 1.4 |QID|141| |N| Fly to Gryan Stoutmantle (56.2, 47.6)| |Z|Westfall|
+T The Defias Brotherhood Part 1.4 |QID|141| |N| Turn-In The Defias Brotherhood Part 1.4 (56.2, 47.6)| |Z|Westfall|
+
+A The Defias Brotherhood Part 1.5 |QID|142| |N| AcceptThe Defias Brotherhood Part 1.5 (56.2, 47.6)| |Z|Westfall|
+R The Defias Brotherhood Part 1.5 |QID|142| |N| Run to respawn point of Defias Messenger (44, 69)| |Z|Westfall|
+C The Defias Brotherhood Part 1.5 |QID|142| |N| Track down the Defias Messenger| |Z|Westfall|
+R The Defias Brotherhood Part 1.5 |QID|142| |N| Run to Gryan Stoutmantle (56.2, 47.6)| |Z|Westfall|
+T The Defias Brotherhood Part 1.5 |QID|142| |N| Turn-In The Defias Brotherhood Part 1.5 (56.2, 47.6)| |Z|Westfall|
+
+R The Defias Brotherhood Part 2.1 |QID|155| |N| Run to The Defias Traitor (55.6, 47.6)| |Z|Westfall|
+A The Defias Brotherhood Part 2.1 |QID|155| |N| Accept The Defias Brotherhood Part 2.1 (55.6, 47.6)| |Z|Westfall|
+C The Defias Brotherhood Part 2.1 |QID|155| |N| Escort The Defias Traitor to discover where VanCleef is hiding| |Z|Westfall|
+R The Defias Brotherhood Part 2.1 |QID|155| |N| Run to Gryan Stoutmantle (56.2, 47.6)| |Z|Westfall|
+T The Defias Brotherhood Part 2.1 |QID|155| |N| Turn-In The Defias Brotherhood Part 2.1 (56.2, 47.6)| |Z|Westfall|
+
+A The Defias Brotherhood Part 2.2 |QID|156| |N| Accept The Defias Brotherhood Part 2.2| |Z|Westfall|
+
+R Red Silk Bandanas |QID|156| |N| Run to Scout Riell (56.6 47.4)| |Z|Westfall| |PRE|942|
+A Red Silk Bandanas |QID|156| |N| Accept Red Silk Bandanas (56.6 47.4)| |Z|Westfall|
+
+R Oh Brother... |QID|167| |N| Run to Wilder Thistlenettle (65.2, 21.2)| |Z|Stormwind City|
+A Oh Brother... |QID|167| |N| Accept Oh Brother... (65.2, 21.2)| |Z|Stormwind City|
+A Collecting Memories |QID|168| |N| Accept Collecting Memories (65.2, 21.2)| |Z|Stormwind City|
+
+
+C Collecting Memories |QID|168| |N| Retrieve 4 Miners' Union Cards and return them to Wilder Thistlenettle in Stormwind City |L|1894 4| |Z|The Deadmines|
+
+C Oh Brother... |QID|167| |N| Bring Foreman Thistlenettle's Explorers' League Badge to Wilder Thistlenettle in Stormwind City| |L|1875| |Z|The Deadmines|
+
+C Red Silk Bandanas |QID|156| |N| Scout Riell at the Sentinel Hill Tower wants you to bring her 10 Red Silk Bandanas |L|915 10| |Z|The Deadmines|
+
+C Underground Assault |QID|2040| |N| Retrieve the Gnoam Sprecklesprocket from the Deadmines| |L|7365| |Z|The Deadmines|
+
+C The Defias Brotherhood Part 2.2 |QID|156| |N| Kill Edwin VanCleef and bring his head to Gryan Stoutmantle| |L|3637| |Z|The Deadmines|
+
+C The Unsent Letter |QID|373| |N| Deliver the Letter to the City Architect to Baros Alexston in Stormwind City| |L|2874| |Z|The Deadmines|
+
+
+R Red Silk Bandanas |QID|156| |N| Run to Scout Riell (56.6 47.4)| |Z|Westfall| 
+T Red Silk Bandanas |QID|156| |N| Turn-In Red Silk Bandanas (56.6 47.4)| |Z|Westfall|
+R Underground Assault |QID|2040| |N| Speak with Shoni the Shilent in Stormwind City and Accept Underground Assault (62.6, 34.1)| |Z|Stormwind City|
+T Underground Assault |QID|2040| |N| Turn-In Underground Assault | |Z|Stormwind City|
+
+R Oh Brother... |QID|167| |N| Run to Wilder Thistlenettle (65.2, 21.2)| |Z|Stormwind City|
+T Oh Brother... |QID|167| |N| Turn-In Oh Brother... (65.2, 21.2)| |Z|Stormwind City|
+T Collecting Memories |QID|168| |N| Collecting Memories (65.2, 21.2)| |Z|Stormwind City|
+
+R The Unsent Letter |QID|373| |N| Run to Baros Alexston (49.0, 30.2)| |Z|Stormwind City|
+T The Unsent Letter |QID|373| |N| Turn-In The Unsent Letter (49.0, 30.2)| |Z|Stormwind City|
+
+
+A Bazil Thredd |QID|389| |N| Accept Bazil Thredd (49.0, 30.2)| |Z|Stormwind City| |PRE|373|
+
+
+N Guide Complete
+
+]]
+end)
+
+
+--[[R GGGG |QID|111111111| |N| Run to the Deadmines (62.6, 34.1)| |Z|Stormwind City |
+
++Underground Assault	+
++The Defias Brotherhood	+
++Red Silk Bandanas+
++Oh Brother. . .+
++Collecting Memories+
++The Unsent Letter+
+
+
+
+
+
+--]]
